5a9d8d8daadf58d786f9fcfdff8ffc7c0f85f829
karming.wan <karming1997@gmail.com>
2022-07-20 21:48:04 +0800

    delete spinarak-serverless

--- spinarak-serverless/src/modules/klse-market/get-historical/request-schema.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  type: "object",
-  properties: {
-    stockcode: { type: 'integer' },
-    fromDate: {type: 'string', format: 'date'},
-    toDate: {type: 'string', format: 'date'}
-  },
-  required: ['stockcode']
-} as const;
